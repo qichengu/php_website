b99cd60cc92570e972f313bd696f7af193006ee1
Qichen Gu <qichengu.opt@gmail.com>
2017-09-17 23:54:56 -0700

    ip logger

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+function logIP()
+{
+    $ipLog="logfile.htm"; // Your logfiles name here (.txt or .html extensions ok)
+
+    // IP logging function by Dave Lauderdale
+    // Originally published at: www.digi-dl.com
+
+    $register_globals = (bool) ini_get('register_gobals');
+    if ($register_globals) $ip = getenv(REMOTE_ADDR);
+    else $ip = $_SERVER['REMOTE_ADDR'];
+
+    $date=date ("l dS of F Y h:i:s A");
+    $log=fopen("$ipLog", "a+");
+
+    if (preg_match("/\\bhtm\\b/i", $ipLog) || preg_match("/\\bhtml\\b/i", $ipLog))
+    {
+        fputs($log, "Logged IP address: $ip - Date logged: $date<br>");
+    }
+    else fputs($log, "Logged IP address: $ip - Date logged: $date\
+");
+
+    fclose($log);
+}
+// Place the below function call wherever you want the script to fire.
+logIp();
+
+
+?>
